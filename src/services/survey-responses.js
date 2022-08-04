0330c5e93927119d6fbe03676571a31e6cf43bd2
varun1311996 <v.kothapally@eagles.oc.edu>
2022-08-05 01:15:59 +0530

    fincode

--- src/services/survey-responses.js
+++ src/services/survey-responses.js
@@ -7,9 +7,10 @@ class SurveyResponse {
       create(name,userId,id, data) {
         return http.post(`/surveys/responses?name=${name}&responder=${userId}&surveyId=${id}`, data);
       }
-    //   get(sid) {
-    //     return axios.get(API_URL+`/${sid}`);
-    //   }
+      get(sid) {
+        return http.get(`/surveys/responses/${sid}`)
+        //return axios.get(API_URL+`/${sid}`);
+      }
 }
 
 export default new SurveyResponse();
